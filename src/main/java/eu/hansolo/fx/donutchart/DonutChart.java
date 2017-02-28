c15675f208bbe053f2c2326b094ef50fc3aa2208
Gerrit Grunwald <han.solo@mac.com>
2017-02-28 09:22:37 +0100

    Added a legend prototype

--- src/main/java/eu/hansolo/fx/donutchart/DonutChart.java
+++ src/main/java/eu/hansolo/fx/donutchart/DonutChart.java
@@ -27,6 +27,7 @@ import javafx.scene.shape.StrokeLineCap;
 import javafx.scene.text.Font;
 import javafx.scene.text.TextAlignment;
 
+import java.util.List;
 import java.util.Locale;
 
 
@@ -46,8 +47,10 @@ public class DonutChart extends Region {
     private              double                        size;
     private              double                        width;
     private              double                        height;
-    private              Canvas                        canvas;
-    private              GraphicsContext               ctx;
+    private              Canvas                        chartCanvas;
+    private              GraphicsContext               chartCtx;
+    private              Canvas                        legendCanvas;
+    private              GraphicsContext               legendCtx;
     private              Pane                          pane;
     private              Paint                         backgroundPaint;
     private              Paint                         borderPaint;
@@ -107,10 +110,13 @@ public class DonutChart extends Region {
             }
         }
 
-        canvas = new Canvas(PREFERRED_WIDTH, PREFERRED_HEIGHT);
-        ctx    = canvas.getGraphicsContext2D();
+        chartCanvas = new Canvas(PREFERRED_WIDTH, PREFERRED_HEIGHT);
+        chartCtx = chartCanvas.getGraphicsContext2D();
 
-        pane = new Pane(canvas);
+        legendCanvas = new Canvas(PREFERRED_WIDTH * 0.225, PREFERRED_HEIGHT);
+        legendCtx    = legendCanvas.getGraphicsContext2D();
+
+        pane = new Pane(legendCanvas, chartCanvas);
         pane.setBackground(new Background(new BackgroundFill(backgroundPaint, CornerRadii.EMPTY, Insets.EMPTY)));
         pane.setBorder(new Border(new BorderStroke(borderPaint, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(borderWidth))));
 
@@ -147,53 +153,80 @@ public class DonutChart extends Region {
     public Color getBarBorderColor() { return barBorderColor.get(); }
     public void setBarBorderColor(final Color COLOR) { barBorderColor.set(COLOR); }
     public ObjectProperty<Color> barBorderColorProperty() { return barBorderColor; }
-
+    
     private void drawChart() {
+        double          canvasSize     = chartCanvas.getWidth();
         int             noOfItems      = dataList.size();
-        double          center         = size * 0.5;
-        double          innerRadius    = size * 0.275;
-        double          outerRadius    = size * 0.4;
-        double          barWidth       = size * 0.1;
+        double          center         = canvasSize * 0.5;
+        double          innerRadius    = canvasSize * 0.275;
+        double          outerRadius    = canvasSize * 0.4;
+        double          barWidth       = canvasSize * 0.1;
         //List<ChartData> sortedDataList = dataList.stream().sorted(Comparator.comparingDouble(ChartData::getValue).reversed()).collect(Collectors.toList());
         double          sum            = dataList.stream().mapToDouble(ChartData::getValue).sum();
         double          stepSize       = 360.0 / sum;
         double          angle          = 0;
         double          startAngle     = 90;
-        double          xy             = size * 0.1;
-        double          wh             = size * 0.8;
+        double          xy             = canvasSize * 0.1;
+        double          wh             = canvasSize * 0.8;
+        Color           bkgColor       = Color.BLACK;
+        Color           textColor      = Color.WHITE;
 
-        ctx.clearRect(0, 0, size, size);
-        ctx.setLineCap(StrokeLineCap.BUTT);
-        ctx.setFill(getTextColor());
-        ctx.setTextAlign(TextAlignment.RIGHT);
-        ctx.setTextBaseline(VPos.CENTER);
-        ctx.setTextAlign(TextAlignment.CENTER);
+        chartCtx.clearRect(0, 0, canvasSize, canvasSize);
+        chartCtx.setLineCap(StrokeLineCap.BUTT);
+        chartCtx.setFill(textColor);
+        chartCtx.setTextBaseline(VPos.CENTER);
+        chartCtx.setTextAlign(TextAlignment.CENTER);
 
-        // Name
-        ctx.setFont(Font.font(size * 0.15));
-        ctx.fillText(String.format(Locale.US, "%.0f", sum), center, center, size * 0.4);
+        // Sum
+        chartCtx.setFont(Font.font(canvasSize * 0.15));
+        chartCtx.fillText(String.format(Locale.US, "%.0f", sum), center, center, canvasSize * 0.4);
 
-        ctx.setFont(Font.font(barWidth * 0.5));
+        chartCtx.setFont(Font.font(barWidth * 0.5));
         for (int i = 0 ; i < noOfItems ; i++) {
             ChartData data  = dataList.get(i);
             double    value = data.getValue();
             startAngle -= angle;
             angle = value * stepSize;
 
-            // DataBar
-            ctx.setLineWidth(barWidth);
-            ctx.setStroke(data.getColor());
-            ctx.strokeArc(xy, xy, wh, wh, startAngle, -angle, ArcType.OPEN);
+            // Segment
+            chartCtx.setLineWidth(barWidth);
+            chartCtx.setStroke(data.getColor());
+            chartCtx.strokeArc(xy, xy, wh, wh, startAngle, -angle, ArcType.OPEN);
 
             // Percentage
             double x = innerRadius * Math.cos(Math.toRadians(startAngle - (angle * 0.5)));
             double y = -innerRadius * Math.sin(Math.toRadians(startAngle - (angle * 0.5)));
-            ctx.fillText(String.format(Locale.US, "%.0f%%", (value / sum * 100.0)), center  + x, center + y, barWidth);
+            chartCtx.setFill(textColor);
+            chartCtx.fillText(String.format(Locale.US, "%.0f%%", (value / sum * 100.0)), center + x, center + y, barWidth);
 
             // Value
             x = outerRadius * Math.cos(Math.toRadians(startAngle - (angle * 0.5)));
             y = -outerRadius * Math.sin(Math.toRadians(startAngle - (angle * 0.5)));
-            ctx.fillText(String.format(Locale.US, "%.0f", value), center  + x, center + y, barWidth);
+            chartCtx.setFill(bkgColor);
+            chartCtx.fillText(String.format(Locale.US, "%.0f", value), center + x, center + y, barWidth);
+        }
+    }
+
+    private void drawLegend() {
+        double          canvasWidth  = legendCanvas.getWidth();
+        double          canvasHeight = legendCanvas.getHeight();
+        int             noOfItems    = dataList.size();
+        //List<ChartData> sortedDataList = dataList.stream().sorted(Comparator.comparingDouble(ChartData::getValue).reversed()).collect(Collectors.toList());
+        Color           textColor    = Color.WHITE;
+        double          stepSize     = canvasHeight * 0.9 / (noOfItems + 1);
+
+        legendCtx.clearRect(0, 0, canvasWidth, canvasHeight);
+        legendCtx.setTextAlign(TextAlignment.LEFT);
+        legendCtx.setTextBaseline(VPos.CENTER);
+        legendCtx.setFont(Font.font(canvasHeight * 0.045));
+
+        for (int i = 0 ; i < noOfItems ; i++) {
+            ChartData data = dataList.get(i);
+
+            legendCtx.setFill(data.getColor());
+            legendCtx.fillOval(0, (i + 1) * stepSize, size * 0.0375, size * 0.0375);
+            legendCtx.setFill(textColor);
+            legendCtx.fillText(data.getName(), size * 0.05, (i + 1) * stepSize + canvasHeight * 0.025);
         }
     }
 
@@ -205,12 +238,24 @@ public class DonutChart extends Region {
         size   = width < height ? width : height;
 
         if (width > 0 && height > 0) {
-            pane.setMaxSize(size, size);
-            pane.setPrefSize(size, size);
-            pane.relocate((getWidth() - size) * 0.5, (getHeight() - size) * 0.5);
+            pane.setMaxSize(width, height);
+            pane.setPrefSize(width, height);
 
-            canvas.setWidth(size);
-            canvas.setHeight(size);
+            chartCanvas.setWidth(size);
+            chartCanvas.setHeight(size);
+
+            legendCanvas.setWidth(size * 0.25);
+            legendCanvas.setHeight(size);
+            legendCanvas.relocate(size * 0.05, size * 0.05);
+            legendCanvas.setVisible(width > (height * 1.2));
+
+            if (width > (height * 1.5)) {
+                chartCanvas.relocate((width - size) * 0.5, 0);
+            } else if (width > (height * 1.2)) {
+                chartCanvas.relocate(width - size, 0);
+            } else {
+                chartCanvas.relocate((width - size) * 0.5, 0);
+            }
 
             redraw();
         }
@@ -220,6 +265,7 @@ public class DonutChart extends Region {
         pane.setBackground(new Background(new BackgroundFill(backgroundPaint, CornerRadii.EMPTY, Insets.EMPTY)));
         pane.setBorder(new Border(new BorderStroke(borderPaint, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(borderWidth / PREFERRED_WIDTH * size))));
 
+        drawLegend();
         drawChart();
     }
 }
